b07edf359fc3302b53782a02d6c5c969827acad0
nashamri <designernasser@gmail.com>
2018-03-17 16:56:20 +0000

    Fixing and improving commentary section

--- academic-phrases.el
+++ academic-phrases.el
@@ -23,19 +23,19 @@
 
 ;;; Commentary:
 
-;; When writing your academic paper, you might get stuck trying to
-;; find the right phrase that captures your intention. This package
-;; tries to alleviate that problem by presenting you with a list of
-;; phrases organized by the topic or by the paper section that you
-;; are writing. This package has around 600 phrases so far.
+;; When writing your academic paper, you might get stuck trying to find the
+;; right phrase that captures your intention. This package tries to alleviate
+;; that problem by presenting you with a list of phrases organized by the topic
+;; or by the paper section that you are writing. This package has around 600
+;; phrases so far.
 
 ;; Using this package is easy, just call `M-x academic-phrases` to
 ;; get a list of phrases organized by topic, or call
 ;; `academic-phrases-by-section` to browse the phrases by the paper
-;; section.
+;; section and fill-in the blanks if required.
 
 ;; This work was based on the freely available PDF titled "English
-;; for Writing Research Papers Useful Phrases" which can be found
+;; for Writing Research - Papers Useful Phrases" which can be found
 ;; here <http://www.springer.com/gb/book/9783319260921>. This work
 ;; was done with the kind permission of Springer Nature and Adrian
 ;; Wallwork.
